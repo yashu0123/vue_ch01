7b43b6729b4c7d2e1d2fac417f65ed52ac0f112f
yashu0123 <yashwantsadh746@gmail.com>
2025-03-17 10:57:53 +0530

    adding project ch01

---
+++ app.js
@@ -0,0 +1,37 @@
+// const options = {
+//     data() {
+//         return {
+//             appName : 'VueApp',
+//             cls : 'cross',
+//             counter : 4,
+//             name: "yashwant"
+//         }
+//     },
+//     methods:{
+//         increment() {
+//             if(this.counter==10)
+//                 this.counter=100;
+//             this.counter++;
+//         }
+//     }
+    
+// }
+// const app = Vue.createApp(options);
+// const vm = app.mount("#app");
+new Vue({
+    el: '#app',
+    data: {
+      title: 'Simple Vue.js Project',
+      description: 'This is a simple one-page Vue.js project with HTML, CSS, and JS.',
+      buttonText: 'Show Message',
+      showMessage: false,
+      message: 'Hello! You clicked the button.',
+    },
+    methods: {
+      toggleMessage() {
+        this.showMessage = !this.showMessage;
+        this.buttonText = this.showMessage ? 'Hide Message' : 'Show Message';
+      }
+    }
+  });
+  
